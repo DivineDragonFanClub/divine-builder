989af702dfac573160c94fb975fcf58e6698cf7b
Raytwo <raytwost@gmail.com>
2024-01-31 16:15:08 +0100

    Add code

---
+++ Editor/Build.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace DivineDragon
+{
+    public class Build
+    {
+        [MenuItem("Divine Dragon/Build Addressables")]
+        public static void BuildAddressables()
+        {
+            BuildAddressableContent();
+        }
+        
+        [MenuItem("Divine Dragon/Build Addressables", true)]
+        static bool ValidateBuildAddressables()
+        {
+            // Return false if no bundle output path is set
+            return !string.IsNullOrEmpty(DivineDragonSettingsScriptableObject.instance.getBundleOutputPath());
+        }
+        
+        public static bool BuildAddressableContent()
+        {
+            AddressableAssetSettings
+                .BuildPlayerContent(out AddressablesPlayerBuildResult result);
+            bool success = string.IsNullOrEmpty(result.Error);
+
+            if (!success)
+            {
+                Debug.LogError("Addressables build error encountered: " + result.Error);
+                return false;
+            }
+            
+            var outputDirectory = DivineDragonSettingsScriptableObject.instance.getBundleOutputPath();
+
+            var args = String.Format("fix \"{0}\" \"{1}\"", outputDirectory, result.OutputPath);
+            
+
+            var bundleTools = "bundle_tools";
+            // get the platform that this editor is running on
+            var platform = Application.platform;
+            
+            // if the platform is windows, append .exe to the bundle_tools name
+            if (platform == RuntimePlatform.WindowsEditor)
+            {
+                bundleTools += ".exe";
+            }
+            
+            RunProcess(bundleTools, false, args);
+            if (DivineDragonSettingsScriptableObject.instance.getOpenAfterBuild())
+            {
+                EditorUtility.RevealInFinder(outputDirectory);
+            }
+
+            return success;
+        }
+        
+        static void RunProcess(string command, bool runShell, string args = null)
+        {
+            string projectCurrentDir = Directory.GetCurrentDirectory();
+            command = projectCurrentDir + "/Assets/" + command;
+ 
+            UnityEngine.Debug.Log(string.Format("{0} Run command: {1}", DateTime.Now, command));
+ 
+            ProcessStartInfo ps = new ProcessStartInfo(command);
+            using (Process p = new Process())
+            {
+                ps.UseShellExecute = runShell;
+                if (!runShell)
+                {
+                    ps.RedirectStandardOutput = true;
+                    ps.RedirectStandardError = true;
+                    ps.StandardOutputEncoding = System.Text.ASCIIEncoding.ASCII;
+                }
+                if (args != null && args != "")
+                {
+                    ps.Arguments = args;
+                }
+                p.StartInfo = ps;
+                p.Start();
+                p.WaitForExit();
+                if (!runShell)
+                {
+                    string output = p.StandardOutput.ReadToEnd().Trim();
+                    if (!string.IsNullOrEmpty(output))
+                    {
+                        // Split output into lines and debug log each line
+                        string[] lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                        foreach (string line in lines)
+                        {
+                            UnityEngine.Debug.Log(string.Format("{0} Output: {1}", DateTime.Now, line));
+                        }
+                    }
+ 
+                    string errors = p.StandardError.ReadToEnd().Trim();
+                    if (!string.IsNullOrEmpty(errors))
+                    {
+                        // Split output into lines and debug log each line
+                        string[] lines = errors.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                        foreach (string line in lines)
+                        {
+                            UnityEngine.Debug.Log(string.Format("{0} Output: {1}", DateTime.Now, line));
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    
+}
---
+++ Editor/DivineDragonSettingsScriptableObject.cs
@@ -0,0 +1,143 @@
+using System;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
+
+namespace DivineDragon
+{
+    [FilePath("Assets/Editor/DivineSettings.settings", FilePathAttribute.Location.ProjectFolder)]
+    public class DivineDragonSettingsScriptableObject : ScriptableSingleton<DivineDragonSettingsScriptableObject>
+    {
+        [SerializeField]
+        string bundleOutputPath;
+
+        [SerializeField] private bool openAfterBuildCheckbox;
+        
+        public String getBundleOutputPath()
+        {
+            return bundleOutputPath;
+        }
+        
+        public void setOpenAfterBuild(bool openAfterBuild)
+        {
+            openAfterBuildCheckbox = openAfterBuild;
+            Save(true);
+        }
+        
+        public bool getOpenAfterBuild()
+        {
+            return openAfterBuildCheckbox;
+        }
+        public void setBundleOutputPath(string path)
+        {
+            bundleOutputPath = path;
+            Save(true);
+        }
+    }
+
+    /// <summary>
+    /// Copied from https://www.kodeco.com/6452218-uielements-tutorial-for-unity-getting-started?page=2
+    /// </summary>
+    public class SettingsWindow: EditorWindow
+    {
+        private TextField bundleOutputPathField;
+        
+        [MenuItem("Divine Dragon/Divine Dragon Window")]
+        public static void ShowSettings()
+        {
+            SettingsWindow wnd = GetWindow<SettingsWindow>();
+            wnd.titleContent = new GUIContent("Divine Dragon Window");
+        }
+        
+        public void OnEnable()
+        {
+            // 3
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+
+            // 5
+            // Import UXML
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>
+                ("Assets/Editor/DivineWindow.uxml");
+            VisualElement divineWindow = visualTree.CloneTree();
+            root.Add(divineWindow);
+            
+            InitializeBundleOutputField(divineWindow);
+            InitializeBrowseButton(divineWindow);
+            InitializeOpenOutputButton(divineWindow);
+            InitializeBuildButton(divineWindow);
+            InitializeOpenAfterBuildCheckbox(divineWindow);
+
+        }
+
+        private void InitializeOpenAfterBuildCheckbox(VisualElement divineWindow)
+        {
+            Toggle openAfterBuildCheckbox = divineWindow.Q<Toggle>("openAfterBuildCheckbox");
+            openAfterBuildCheckbox.value = DivineDragonSettingsScriptableObject.instance.getOpenAfterBuild();
+            openAfterBuildCheckbox.RegisterValueChangedCallback(evt =>
+            {
+                DivineDragonSettingsScriptableObject.instance.setOpenAfterBuild(evt.newValue);
+            });
+        }
+
+        private void InitializeBundleOutputField(VisualElement divineWindow)
+        {
+            TextField bundleOutputField = divineWindow.Q<TextField>("BundleOutputField");
+            
+            bundleOutputPathField = bundleOutputField;
+            bundleOutputField.value = DivineDragonSettingsScriptableObject.instance.getBundleOutputPath();
+            
+            // reflect edits of the field back to the scriptable object
+            bundleOutputField.RegisterValueChangedCallback(evt =>
+            {
+                DivineDragonSettingsScriptableObject.instance.setBundleOutputPath(evt.newValue);
+            });
+        }
+        
+        private void InitializeBrowseButton(VisualElement divineWindow)
+        {
+            Button browseButton = divineWindow.Q<Button>("BrowseOutputPath");
+            
+            browseButton.clickable.clicked += () =>
+            {
+                var outputPath = EditorUtility.OpenFolderPanel("Choose a folder to save the output to", "", "");
+                if (string.IsNullOrEmpty(outputPath))
+                {
+                    Debug.Log("no path to output?");
+                    return;
+                }
+                DivineDragonSettingsScriptableObject.instance.setBundleOutputPath(outputPath);
+                bundleOutputPathField.value = outputPath;
+                Debug.Log("Set the output path to " + DivineDragonSettingsScriptableObject.instance.getBundleOutputPath());
+            };
+        }
+        
+        private void InitializeOpenOutputButton(VisualElement divineWindow)
+        {
+            Button openOutputButton = divineWindow.Q<Button>("OpenOutputPath");
+            
+            openOutputButton.clickable.clicked += () =>
+            {
+                EditorUtility.OpenWithDefaultApp(DivineDragonSettingsScriptableObject.instance.getBundleOutputPath());
+            };
+        }
+        
+        private void InitializeBuildButton(VisualElement divineWindow)
+        {
+            Button buildButton = divineWindow.Q<Button>("BuildAddressablesDivine");
+            
+            buildButton.clickable.clicked += () =>
+            {
+                if (String.IsNullOrEmpty(DivineDragonSettingsScriptableObject.instance.getBundleOutputPath()))
+                {
+                    EditorUtility.DisplayDialog("Error", "No output path set - please set one in this window", "OK");
+                    return;
+                }
+                Build.BuildAddressableContent();
+            };
+        }
+
+    }
+}
