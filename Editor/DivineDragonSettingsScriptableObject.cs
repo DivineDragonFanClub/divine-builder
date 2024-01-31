9e8bf8978ba592b3dd058e24bf95588c89848641
Raytwo <Raytwo@users.noreply.github.com>
2024-01-31 17:14:46 +0100

    Update DivineDragonSettingsScriptableObject.cs

--- Editor/DivineDragonSettingsScriptableObject.cs
+++ Editor/DivineDragonSettingsScriptableObject.cs
@@ -60,7 +60,7 @@ namespace DivineDragon
             // 5
             // Import UXML
             var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>
-                ("Assets/Editor/DivineWindow.uxml");
+                ("Packages/com.unity.divine-builder/Editor/DivineWindow.uxml");
             VisualElement divineWindow = visualTree.CloneTree();
             root.Add(divineWindow);
             
